be14ae4e3d5fe8db616fed60e22dfb78b29ebf7f
CZ <wngus0223@naver.com>
2023-12-15 00:37:27 +0900

    feat: global theme color

---
+++ src/Theme/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  deepBlue: '#275B92',
+  blue: '#598dc4',
+  lightBlue: '#85b4e5',
+  whtie: '#ffffff',
+  black: '#000000',
+  gray: '#f2f2f2',
+  darkGray: '#989ea1',
+};
+
+export default theme;
